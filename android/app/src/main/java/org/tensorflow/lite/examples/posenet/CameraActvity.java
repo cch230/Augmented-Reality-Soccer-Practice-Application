d22dace579fbc1c75ba2ad9488c6d7ca55377530
cch230 <cch230@naver.com>
2020-03-28 18:05:48 +0900

    모듈재정렬&골격인식 추가
    모듈재정렬&골격인식 추가

---
+++ android/app/src/main/java/org/tensorflow/lite/examples/posenet/CameraActvity.java
@@ -0,0 +1,51 @@
+package org.tensorflow.lite.examples.posenet;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.android.JavaCameraView;
+import org.opencv.core.Size;
+
+public class CameraActvity extends AppCompatActivity  {
+    public static final String TAG = "src";
+    private JavaCameraView cameraView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tfe_pn_activity_camera);
+        cameraView = (JavaCameraView) findViewById(R.id.surfaceView);
+        loaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, new PosenetActivity())
+                    .commit();
+        }
+    }
+    private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+}
