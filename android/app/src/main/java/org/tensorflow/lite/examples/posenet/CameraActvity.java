5fd3b937243adc9b700cc3c94e74de84a814473f
cch230 <cch230@naver.com>
2020-05-04 18:43:19 +0900

    Add permissiom & Fix some code (퍼미션 추가& 약각의 수정)
    Add permissiom & Fix some code (퍼미션 추가& 약각의 수정)

--- android/app/src/main/java/org/tensorflow/lite/examples/posenet/CameraActvity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/posenet/CameraActvity.java
@@ -1,5 +1,9 @@
 package org.tensorflow.lite.examples.posenet;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -17,6 +21,7 @@ import org.opencv.core.Size;
 public class CameraActvity extends AppCompatActivity  {
     public static final String TAG = "src";
     private JavaCameraView cameraView;
+    private static final int REQUEST_USED_PERMISSION = 200;
 
     static {
         if (!OpenCVLoader.initDebug()) {
@@ -24,9 +29,22 @@ public class CameraActvity extends AppCompatActivity  {
         }
     }
 
+    private static final String[] needPermissions = {
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        for (String permission : needPermissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, needPermissions, REQUEST_USED_PERMISSION);
+                break;
+            }
+        }
         setContentView(R.layout.tfe_pn_activity_camera);
         cameraView = (JavaCameraView) findViewById(R.id.surfaceView);
         loaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
@@ -35,6 +53,7 @@ public class CameraActvity extends AppCompatActivity  {
                     .replace(R.id.container, new PosenetActivity())
                     .commit();
         }
+
     }
     private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -51,5 +70,26 @@ public class CameraActvity extends AppCompatActivity  {
             }
         }
     };
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        boolean permissionToRecordAccepted = true;
+
+        switch (requestCode){
+            case REQUEST_USED_PERMISSION:
+                for (int result : grantResults) {
+                    if (result != PackageManager.PERMISSION_GRANTED) {
+                        permissionToRecordAccepted = false;
+                        break;
+                    }
+                }
+                break;
+        }
+
+        if (permissionToRecordAccepted == false){
+            finish();
+        }
+    }
 
 }
