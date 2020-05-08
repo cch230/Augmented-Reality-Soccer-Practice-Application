b96e42a3f4db7fcdc60fa4014fa004c1a54c6c6a
기열 이 <11james@kpu.ac.kr>
2020-05-09 05:19:50 +0900

    video upload php

--- server php/videoupload.php
+++ server php/videoupload.php
@@ -1,7 +1,7 @@
 <?php
  require "conn.php";
 
- $user_nickname = $_POST["input_sineup_id"];
+ $user_nickname = $_POST["input_id"];
  $video = $_POST["editTextUpload"];
 
  $isValidVideo = filter_var($video, FILTER_VALIDATE_Video);
@@ -12,7 +12,7 @@
         echo "업로드할 영상을 선택해주세요";
     } else {
 
-	    $sql_upload = "INSERT INTO videoboard (user_nickname, video)VALUES('$user_nickname', '$video')";
+	    $sql_upload = "INSERT INTO videoboard (user_nickname, video)VALUES($user_nickname', '$video')";
 
             if (mysqli_query($conn, $sql_upload)) {
                 echo "성공적으로 업로드했습니다";
