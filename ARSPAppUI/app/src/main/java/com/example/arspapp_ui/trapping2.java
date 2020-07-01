c126422a9e30416a7797b8b657013458c73a6347
cch230 <cch230@naver.com>
2020-07-01 11:16:40 +0900

    Internal modification
    Internal modification

--- ARSPAppUI/app/src/main/java/com/example/arspapp_ui/trapping2.java
+++ ARSPAppUI/app/src/main/java/com/example/arspapp_ui/trapping2.java
@@ -3,15 +3,19 @@ package com.example.arspapp_ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Camera;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
+
 public class trapping2 extends AppCompatActivity {
-    private Spinner spinner2;
     ImageButton trapping2_btn;
+    private Spinner spinner2;
+    private String text;
+
 
 
     @Override
@@ -20,18 +24,22 @@ public class trapping2 extends AppCompatActivity {
         setContentView(R.layout.activity_trapping2);
 
 
-            Spinner spinner2 = findViewById(R.id.spinner2);
+            spinner2 = findViewById(R.id.spinner2);
+
             trapping2_btn = findViewById(R.id.trapping2_btn);
 
             final String[] data = getResources().getStringArray(R.array.trapping_array);
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,data);
             spinner2.setAdapter(adapter);
 
+
     }
     public void onClick(View v) {
         Intent intent;
+        text = spinner2.getSelectedItem().toString();
         if (v==trapping2_btn) {
-            intent = new Intent(trapping2.this, mainmenu.class);
+            intent = new Intent(trapping2.this, trapping.class);
+            intent.putExtra("text", text);
             startActivity(intent);
         }
     }
