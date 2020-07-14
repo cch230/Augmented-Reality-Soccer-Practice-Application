333c8e18a5c445b7a77bdfa5d37cb0b878f0e169
cch230 <cch230@naver.com>
2020-07-15 01:40:54 +0900

    Add setting profile
    Add setting profile

--- ARSPAppUI/app/src/main/java/com/example/arspapp_ui/profile.java
+++ ARSPAppUI/app/src/main/java/com/example/arspapp_ui/profile.java
@@ -2,39 +2,41 @@ package com.example.arspapp_ui;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import javax.xml.transform.Result;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
 
 public class profile extends Fragment {
+    private static final String TAG = "profile";
     public boolean readStoragePermission;
     private static final int REQUEST_CODE = 0;
     ImageView empty_picture;
@@ -42,14 +44,11 @@ public class profile extends Fragment {
     public ImageButton get_frame1_btn;
     View view;
 
-    // @Override
-    // public void onCreate(Bundle savedInstanceState) {
-    //    super.onCreate(savedInstanceState);
-    //  getContext(R.layout.activity_profile);}
-    public profile() {
-
-    }
-
+    private String mTmpDownloadImageUri; //Shared에서 받아올떄 String형이라 임시로 받아오는데 사용
+    private Bitmap img; //비트맵 프로필사진 (이걸
+    final static int PICK_IMAGE = 1; //갤러리에서 사진선택
+    final static int CAPTURE_IMAGE = 2;  //카메라로찍은 사진선택
+    private String mCurrentPhotoPath; //카메라로 찍은 사진 저장할 루트경로
 
     @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -90,58 +89,141 @@ public class profile extends Fragment {
         empty_picture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, REQUEST_CODE);
+              photoDialogRadio();
             }
         });
         return view;
 
     }
 
+    private void photoDialogRadio() {
+        final CharSequence[] PhotoModels = {"앨범에서 사진선택", "카메라로 촬영 후 가져오기", "기본사진으로 하기"};
+        AlertDialog.Builder alt_bld = new AlertDialog.Builder(this.getContext());
+        //alt_bld.setIcon(R.drawable.icon);
+        alt_bld.setTitle("프로필 사진");
+        alt_bld.setSingleChoiceItems(PhotoModels, -1, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                Toast.makeText(getContext(),
+                        PhotoModels[item] + "가 선택되었습니다.",
+                        Toast.LENGTH_SHORT).show();
+                if (item == 0) { //갤러리
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, PICK_IMAGE);
+                } else if (item == 1) { //카메라찍은 사진가져오기
+                    takePictureFromCameraIntent();
+                } else { //기본화면으로하기
+                    empty_picture.setImageResource(R.drawable.ic_person_black_24dp);
+                    img = null;
+                    mTmpDownloadImageUri = null;
+                }
+            }
+        });
+        AlertDialog alert = alt_bld.create();
+        alert.show();
+    }
 
-   /* public void checkSelfPermission() {
-        String temp = "";
+     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+            try {
+                InputStream in = getActivity().getContentResolver().openInputStream(data.getData());
+                img = BitmapFactory.decodeStream(in);
+                in.close();
+                // 이미지 표시
+                ExifInterface ei = new ExifInterface(mCurrentPhotoPath);
+                empty_picture.setImageBitmap(img);
+                Log.d(TAG, "갤러리 inputStream: "+ data.getData() );
+                Log.d(TAG, "갤러리 사진decodeStream: "+img );
 
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
-        }
-
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
-        }
-
-        if (TextUtils.isEmpty(temp) == false) {
-            ActivityCompat.requestPermissions(getActivity(), temp.trim().split(" "), 1);
-        } else {
-            Toast.makeText(getContext(), "권한을 모두 허용", Toast.LENGTH_SHORT).show();
-        }
-
-    }*/
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE) {
+                mTmpDownloadImageUri = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == CAPTURE_IMAGE && resultCode == Activity.RESULT_OK) {
             if (resultCode == RESULT_OK) {
                 try {
-                    InputStream in = getActivity().getContentResolver().openInputStream(data.getData());
-                    Bitmap img = BitmapFactory.decodeStream(in);
-                    in.close();
-                    empty_picture.setImageBitmap(img);
+                    File file = new File(mCurrentPhotoPath);
+                    InputStream in =  getActivity().getContentResolver().openInputStream(Uri.fromFile(file));
+                    img = BitmapFactory.decodeStream(in);
+                    ExifInterface ei = new ExifInterface(mCurrentPhotoPath);
+                    int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+                    Bitmap rotatedBitmap = null;
+                    switch(orientation) {
 
+                        case ExifInterface.ORIENTATION_ROTATE_90:
+                            rotatedBitmap = rotateImage(img, 90);
+                            break;
+
+                        case ExifInterface.ORIENTATION_ROTATE_180:
+                            rotatedBitmap = rotateImage(img, 180);
+                            break;
+
+                        case ExifInterface.ORIENTATION_ROTATE_270:
+                            rotatedBitmap = rotateImage(img, 270);
+                            break;
+
+                        case ExifInterface.ORIENTATION_NORMAL:
+                        default:
+                            rotatedBitmap = img;
+                    }
+                    empty_picture.setImageBitmap(rotatedBitmap);
+                    in.close();
+
+                    mTmpDownloadImageUri = null;
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-
-            } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(getContext(), "사진 선택 취소", Toast.LENGTH_LONG).show();
             }
         }
-
-
     }
 
+    private void takePictureFromCameraIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity( getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getContext(),
+                        "com.example.arspapp_ui.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAPTURE_IMAGE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir;
+        storageDir =  getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
 }
 
 
